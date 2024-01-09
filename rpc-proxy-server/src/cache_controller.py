64bfa0c140af7adde36518bbb932290b964ef3d8
alb-le <alb.leao@gmail.com>
2024-01-09 09:32:48 -0300

    add redis as cache

--- rpc-proxy-server/src/cache_controller.py
+++ rpc-proxy-server/src/cache_controller.py
@@ -4,7 +4,7 @@ from typing import Dict, List
 
 class CacheController:
     def __init__(self, host: str, port: int, decode_responses: bool = True):
-        _r = redis.Redis(host=host, port=port, decode_responses=decode_responses)
+        self._r = redis.Redis(host=host, port=port, decode_responses=decode_responses)
 
     @staticmethod
     def __get_call_signature_as_str(request) -> str:
@@ -16,6 +16,8 @@ class CacheController:
     def get(self, request: List) -> str:
         signature = self.__get_call_signature_as_str(request)
         res = self._r.get(signature + ":1")
+        if res:
+            print(f"[INFO] Got from cache: {request} -> {res}.")
         return res
 
     def set(self, request: List, res: str):
